bc395051ef7c2ccc4d0fd54a03fd799e27f28d36
mucoolr <83160436+mucoolr@users.noreply.github.com>
2021-09-20 17:34:19 +0530

    Update outputs.tf

--- terraform/Load_Balancer/outputs.tf
+++ terraform/Load_Balancer/outputs.tf
@@ -2,6 +2,6 @@ output "tgARN" {
    value = aws_lb_target_group.target_group.arn
  }
 
-output "alb_DNS"{
-   value = data.aws_lb.data_lb.dns_name
+output "lb_dns_name"{
+   value = module.Load_Balancer.lb_dns_name
 }
