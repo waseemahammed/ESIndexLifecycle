94cc5176bfbf8e4430a8bec9d0551f42855b750a
mucoolr <83160436+mucoolr@users.noreply.github.com>
2021-09-23 15:22:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,27 @@
 module "Load_Balancer" {
-  source = "./Load_Balancer"
-  lBsecurityGroups=["sg-00dabe59749aa1fa2"]
-  lBSubnet=["subnet-06ccdf43963cdf2e2","subnet-0ddb8f7753d452e9e"]
-  vpcId      = "vpc-0d3925dbda8fa5661"
-  ecsClusterName = "config-service-cluster"
-  
+  source           = "./Load_Balancer"
+  lBsecurityGroups = var.lBsecurityGroups
+  lBSubnet         = var.lBSubnet
+  vpcId            = var.vpcId
+  ecsClusterName   = var.ecsClusterName
+  tags             = var.tags
+
 
 }
 module "ECS" {
-  source = "./ECS"
-  
-  securityGroups=["sg-00dabe59749aa1fa2"]
-  subnets=["subnet-06ccdf43963cdf2e2"]
-  vpcId      = "vpc-0d3925dbda8fa5661"
-  ecsClusterName = "config-service-cluster"
-  name_ecs_service="config_service"
-  taskDesiredCount = 1
-  imageURI = "630532976899.dkr.ecr.us-east-1.amazonaws.com/gokloudwebsite:first" 
-  cpu = 256
-  memory = 512
-  roleARN = "arn:aws:iam::630532976899:role/ecsTaskExecutionRole"
-  container_name="config_service_container" 
-  containerPort = 80
-  tgARN= module.Load_Balancer.tgARN
+  source           = "./ECS"
+  securityGroups   = var.ecs_securityGroups
+  subnets          = var.ecs_subnets
+  vpcId            = var.vpcId
+  ecsClusterName   = var.ecsClusterName
+  name_ecs_service = var.name_ecs_service
+  taskDesiredCount = var.taskDesiredCount
+  imageURI         = var.imageURI
+  cpu              = var.cpu
+  memory           = var.memory
+  roleARN          = var.roleARN
+  container_name   = var.container_name
+  containerPort    = var.containerPort
+  tgARN            = var.tgARN
+  tags             = var.tags
 }
